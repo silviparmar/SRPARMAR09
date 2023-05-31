9d076980838f542479ecd531e475f8fbc1cce22d
silviparmar <133185468+silviparmar@users.noreply.github.com>
2023-05-31 13:44:03 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,14 @@
+package stu_java;
+
+public class student {
+	public static void main(String s[])
+	{
+		student1 s1=new student1();
+		s1.setName("Shruti");
+		s1.setID(001);
+		s1.setAge(22);
+		s1.setNo(6546);
+		System.out.println(s1.getName()+"'s ID is "+s1.getID()+" age is "+s1.getAge()+" Mobile No  is "+s1.getNo());
+	}
+
+}
---
+++ student1.java
@@ -0,0 +1,52 @@
+package stu_java;
+
+public class student1 
+	{
+		private String Name;
+		private int ID;
+		private int Age;
+		private int No;
+		
+		//NAME
+		public String getName()
+		{
+			return Name;
+		}
+		
+		public void setName(String n)
+		{
+			Name=n;
+		}
+		//ID
+		public int getID()
+		{
+			return ID;
+		}
+		
+		public void setID(int x)
+		{
+			ID=x;
+		}
+		//AGE
+		public int getAge()
+		{
+			return Age;
+		}
+		
+		public void setAge(int x1)
+		{
+			Age=x1;
+		}
+		//MOBILE NO
+		public int getNo()
+		{
+			return No;
+		}
+		
+		public void setNo(int x2)
+		{
+			No=x2;
+		}
+	}
+
+
